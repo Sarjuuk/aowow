973d2d90aa56eb5d5a9092eb61356c308c3a17e6
Toshik <toshik@toshik.com>
2015-09-10 11:51:15 +0300

    Quest DB structure fix
    Fixes related to latest DB structure changes:

--- setup/tools/sqlgen/quests.func.php
+++ setup/tools/sqlgen/quests.func.php
@@ -25,13 +25,13 @@ function quests(array $ids = [])
     $baseQuery = '
         SELECT
             q.ID,
-            Method,
+            QuestType AS Method,
             QuestLevel,
             MinLevel,
             MaxLevel,
             QuestSortID,
             QuestSortID AS zoneOrSortBak,                   -- ZoneOrSortBak
-            QuestType,
+            QuestInfoID AS QuestType,
             SuggestedGroupNum,
             LimitTime,
             IFNULL(gesqr.eventEntry, 0) AS eventId,
@@ -76,8 +76,8 @@ function quests(array $ids = [])
             LogDescription,             Objectives_loc2,            Objectives_loc3,            Objectives_loc6,            Objectives_loc8,
             QuestDescription,           Details_loc2,               Details_loc3,               Details_loc6,               Details_loc8,
             EndText,                    EndText_loc2,               EndText_loc3,               EndText_loc6,               EndText_loc8,
-            OfferRewardText,            OfferRewardText_loc2,       OfferRewardText_loc3,       OfferRewardText_loc6,       OfferRewardText_loc8,
-            RequestItemsText,           RequestItemsText_loc2,      RequestItemsText_loc3,      RequestItemsText_loc6,      RequestItemsText_loc8,
+            qor.RewardText AS OfferRewardText,            OfferRewardText_loc2,       OfferRewardText_loc3,       OfferRewardText_loc6,       OfferRewardText_loc8,
+            qri.CompletionText AS RequestItemsText,           RequestItemsText_loc2,      RequestItemsText_loc3,      RequestItemsText_loc6,      RequestItemsText_loc8,
             QuestCompletionLog,         CompletedText_loc2,         CompletedText_loc3,         CompletedText_loc6,         CompletedText_loc8,
             RequiredNpcOrGo1,           RequiredNpcOrGo2,           RequiredNpcOrGo3,           RequiredNpcOrGo4,
             RequiredNpcOrGoCount1,      RequiredNpcOrGoCount2,      RequiredNpcOrGoCount3,      RequiredNpcOrGoCount4,
@@ -93,6 +93,10 @@ function quests(array $ids = [])
             quest_template q
         LEFT JOIN
             quest_template_addon qa ON q.ID = qa.ID
+        LEFT JOIN
+            quest_offer_reward qor ON q.ID = qor.ID
+        LEFT JOIN
+            quest_request_items qri ON q.ID = qri.ID
         LEFT JOIN
             locales_quest lq ON q.ID = lq.Id
         LEFT JOIN
