Sarjuuk/aowow

commit d83f370fb4f7740eaece8ef19aed1cacffadc989

Page/Maintenance  * send appropriate status code: 503 (temporarily unavailable)
thx @Magnifikator for the heads up

pages/genericPage.class.php
@@ -482,7 +482,8 @@ class GenericPage
         if (isset($this->tabId))
             $this->pageTemplate['activeTab'] = $this->tabId;
 
-        header("HTTP/1.0 404 Not Found", true, 404);
+        header('HTTP/1.0 404 Not Found', true, 404);
+
         $this->display('list-page-generic');
         exit();
     }
@@ -500,13 +501,17 @@ class GenericPage
 
         Util::arraySumByKey($this->mysql, DB::Aowow()->getStatistics(), DB::World()->getStatistics());
 
-        header("HTTP/1.0 404 Not Found", true, 404);
+        header('HTTP/1.0 404 Not Found', true, 404);
+
         $this->display('list-page-generic');
         exit();
     }
 
     public function maintenance()                           // display brb gnomes
     {
+        header('HTTP/1.0 503 Service Temporarily Unavailable', true, 503);
+        header('Retry-After: '.(3 * HOURS));
+
         $this->display('maintenance');
         exit();
     }

commit f2fce0f6021699dea683f67db05495606988a592

Misc/Typos  * fixed typo/php-warning introduced in d83f370fb4f7740eaece8ef19aed1cacffadc989

pages/genericPage.class.php
@@ -510,7 +510,7 @@ class GenericPage
     public function maintenance()                           // display brb gnomes
     {
         header('HTTP/1.0 503 Service Temporarily Unavailable', true, 503);
-        header('Retry-After: '.(3 * HOURS));
+        header('Retry-After: '.(3 * HOUR));
 
         $this->display('maintenance');
         exit();