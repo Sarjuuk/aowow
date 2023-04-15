64413d66cd34eed7ce926948fed0bc9cb8797d5f
Endalaust <root@packer-output-64151668-5ebc-4047-97a8-a341d8730a38>
2023-04-15 20:41:27 +0200

    Update AoWow for woehead

--- static/js/locale_frfr.js
+++ static/js/locale_frfr.js
@@ -1024,7 +1024,6 @@ var mn_community = [
 ];
 var mn_more = [
 	[4,"Retourner sur Way of Elendil","https://way-of-elendil.fr"],
-
     [,"Tout sur Woehead"],
     [0,"À propos / contact","?aboutus"],
     [3,"FAQ","?faq"],
