37def70f6aa6e4341e0f6f5a9a76e13d0b681748
Sarjuuk <junkdump@gmx.net>
2024-07-08 16:46:02 +0200

    Setup/Realms  * force use of Locale EN instead of last used locale (usually RU)

--- setup/tools/filegen/realmmenu.ss.php
+++ setup/tools/filegen/realmmenu.ss.php
@@ -74,6 +74,9 @@ CLISetup::registerSetup("build", new class extends SetupScript
             CLI::write('[realmmenu] no viable realms found .. realm menu will be empty', CLI::LOG_WARN);
 
         // why is this file not localized!?
+        User::useLocale(LOCALE_EN);
+        Lang::load(LOCALE_EN);
+
         foreach (Util::$regions as $idx => $n)
             if ($set & (1 << $idx))
                 $menu[] = [$n, Lang::profiler('regions', $n), null, &$subs[$idx]];
