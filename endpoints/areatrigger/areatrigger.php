1672883186cea4ad551fe1c0d0b422e3477ad95f
Sarjuuk <junkdump@gmx.net>
2025-08-13 19:23:10 +0200

    Template/Update (Part 34)  * convert dbtype 'areatrigger'

---
+++ endpoints/areatrigger/areatrigger.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Aowow;
+
+if (!defined('AOWOW_REVISION'))
+    die('illegal access');
+
+
+class AreatriggerBaseResponse extends TemplateResponse implements ICache
+{
+    use TrDetailPage, TrCache;
+
+    protected  int    $cacheType         = CACHE_TYPE_PAGE;
+    protected  int    $requiredUserGroup = U_GROUP_STAFF;
+
+    protected  string $template          = 'detail-page-generic';
+    protected  string $pageName          = 'areatrigger';
+    protected ?int    $activeTab         = parent::TAB_DATABASE;
+    protected  array  $breadcrumb        = [0, 102];
+
+    public int $type   = Type::AREATRIGGER;
+    public int $typeId = 0;
+
+    private AreaTriggerList $subject;
+
+    public function __construct(string $id)
+    {
+        parent::__construct($id);
+
+        $this->typeId     = intVal($id);
+        $this->contribute = Type::getClassAttrib($this->type, 'contribute') ?? CONTRIBUTE_NONE;
+    }
+
+    protected function generate() : void
+    {
+        $this->subject = new AreaTriggerList(array(['id', $this->typeId]));
+        if ($this->subject->error)
+            $this->generateNotFound(Lang::game('areatrigger'), Lang::areatrigger('notFound'));
+
+        $this->h1 = $this->subject->getField('name') ?: 'Areatrigger #'.$this->typeId;
+
+        $this->gPageInfo += array(
+            'type'   => $this->type,
+            'typeId' => $this->typeId,
+            'name'   => $this->h1
+        );
+
+
+        /*************/
+        /* Menu Path */
+        /*************/
+
+        $this->breadcrumb[] = $this->subject->getField('type');
+
+
+        /**************/
+        /* Page Title */
+        /**************/
+
+        array_unshift($this->title, $this->h1, Util::ucFirst(Lang::game('areatrigger')));
+
+
+        /****************/
+        /* Main Content */
+        /****************/
+
+        $_type = $this->subject->getField('type');
+
+        // get spawns
+        if ($spawns = $this->subject->getSpawns(SPAWNINFO_FULL))
+        {
+            $this->addDataLoader('zones');
+            $this->map = array(
+                ['parent' => 'mapper-generic'],             // Mapper
+                $spawns,                                    // mapperData
+                null,                                       // ShowOnMap
+                [Lang::areatrigger('foundIn')]              // foundIn
+            );
+            foreach ($spawns as $areaId => $_)
+                $this->map[3][$areaId] = ZoneList::getName($areaId);
+        }
+
+        // Smart AI
+        if ($_type == AT_TYPE_SMART)
+        {
+            $sai = new SmartAI(SmartAI::SRC_TYPE_AREATRIGGER, $this->typeId, ['teleportTargetArea' => $this->subject->getField('areaId')]);
+            if ($sai->prepare())
+            {
+                $this->extendGlobalData($sai->getJSGlobals());
+                $this->smartAI = $sai->getMarkup();
+            }
+        }
+
+        $this->redButtons = array(
+            BUTTON_LINKS   => false,
+            BUTTON_WOWHEAD => false
+        );
+
+
+        /**************/
+        /* Extra Tabs */
+        /**************/
+
+        $this->lvTabs = new Tabs(['parent' => "\$\$WH.ge('tabs-generic')"], 'tabsRelated', true);
+
+        // tab: conditions
+        $cnd = new Conditions();
+        $cnd->getBySourceEntry($this->typeId, Conditions::SRC_AREATRIGGER_CLIENT)->prepare();
+        if ($tab = $cnd->toListviewTab())
+        {
+            $this->extendGlobalData($cnd->getJsGlobals());
+            $this->lvTabs->addDataTab(...$tab);
+        }
+
+        if ($_type == AT_TYPE_OBJECTIVE)
+        {
+            $relQuest = new QuestList(array(['id', $this->subject->getField('quest')]));
+            if (!$relQuest->error)
+            {
+                $this->extendGlobalData($relQuest->getJSGlobals(GLOBALINFO_SELF | GLOBALINFO_REWARDS));
+                $this->lvTabs->addListviewTab(new Listview(['data' => $relQuest->getListviewData()], QuestList::$brickFile));
+            }
+        }
+        else if ($_type == AT_TYPE_TELEPORT)
+        {
+            $relZone = new ZoneList(array(['id', $this->subject->getField('areaId')]));
+            if (!$relZone->error)
+                $this->lvTabs->addListviewTab(new Listview(['data' => $relZone->getListviewData()], ZoneList::$brickFile));
+        }
+        else if ($_type == AT_TYPE_SCRIPT)
+        {
+            $relTrigger = new AreaTriggerList(array(['id', $this->typeId, '!'], ['name', $this->subject->getField('name')]));
+            if (!$relTrigger->error)
+                $this->lvTabs->addListviewTab(new Listview(['data' => $relTrigger->getListviewData(), 'name' => Util::ucFirst(Lang::game('areatrigger'))]), AreaTriggerList::$brickFile, 'areatrigger');
+        }
+
+        parent::generate();
+    }
+}
+
+?>
---
+++ endpoints/areatriggers/areatriggers.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Aowow;
+
+if (!defined('AOWOW_REVISION'))
+    die('illegal access');
+
+
+class AreatriggersBaseResponse extends TemplateResponse implements ICache
+{
+    use TrListPage, TrCache;
+
+    protected  int    $type              = Type::AREATRIGGER;
+    protected  int    $cacheType         = CACHE_TYPE_PAGE;
+    protected  int    $requiredUserGroup = U_GROUP_STAFF;
+
+    protected  string $template          = 'areatriggers';
+    protected  string $pageName          = 'areatriggers';
+    protected ?int    $activeTab         = parent::TAB_DATABASE;
+    protected  array  $breadcrumb        = [0, 102];
+
+    protected  array  $scripts           = [[SC_JS_FILE, 'js/filters.js']];
+    protected  array  $expectedGET       = ['filter' => ['filter' => FILTER_VALIDATE_REGEXP, 'options' => ['regexp' => Filter::PATTERN_PARAM]]];
+    protected  array  $validCats         = [0, 1, 2, 3, 4, 5];
+
+    public function __construct(string $pageParam)
+    {
+        $this->getCategoryFromUrl($pageParam);
+
+        if (isset($this->category[0]))
+            $this->forward('?areatriggers&filter=ty='.$this->category[0]);
+
+        parent::__construct($pageParam);
+
+        $this->filter = new AreaTriggerListFilter($this->_get['filter'] ?? '');
+        $this->filterError = $this->filter->error;
+    }
+
+    protected function generate() : void
+    {
+        $this->h1 = Util::ucFirst(Lang::game('areatriggers'));
+
+        $this->filter->evalCriteria();
+
+        $fiForm = $this->filter->values;
+
+
+        /**************/
+        /* Page Title */
+        /**************/
+
+        array_unshift($this->title, $this->h1);
+
+        if (count($fiForm['ty']) == 1)
+            array_unshift($this->title, Lang::areatrigger('types', $fiForm['ty'][0]));
+
+
+        /*************/
+        /* Menu Path */
+        /*************/
+
+        if (count($fiForm['ty']) == 1)
+            $this->breadcrumb[] = $fiForm['ty'];
+
+
+        /****************/
+        /* Main Content */
+        /****************/
+
+        $this->redButtons[BUTTON_WOWHEAD] = false;
+
+        $conditions = [];
+        if ($_ = $this->filter->getConditions())
+            $conditions[] = $_;
+
+        $this->filterError = $this->filter->error;          // maybe the evalX() caused something
+
+        $tabData = [];
+        $trigger = new AreaTriggerList($conditions, ['calcTotal' => true]);
+        if (!$trigger->error)
+        {
+            $tabData['data'] = $trigger->getListviewData();
+
+            // create note if search limit was exceeded; overwriting 'note' is intentional
+            if ($trigger->getMatches() > Cfg::get('SQL_LIMIT_DEFAULT'))
+            {
+                $tabData['note'] = sprintf(Util::$tryFilteringEntityString, $trigger->getMatches(), '"'.Lang::game('areatriggers').'"', Cfg::get('SQL_LIMIT_DEFAULT'));
+                $tabData['_truncated'] = 1;
+            }
+        }
+
+        $this->lvTabs = new Tabs(['parent' => "\$\$WH.ge('tabs-generic')"]);
+
+        $this->lvTabs->addListviewTab(new Listview($tabData, AreaTriggerList::$brickFile, 'areatrigger'));
+
+        parent::generate();
+    }
+}
+
+?>
