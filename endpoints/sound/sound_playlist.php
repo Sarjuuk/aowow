040786df19f0f760652a9d87db9d60eeac475959
Sarjuuk <junkdump@gmx.net>
2025-08-13 23:12:34 +0200

    Template/Update (Part 37)  * convert dbtype 'sound'  * TODO - modernise js

---
+++ endpoints/sound/sound_playlist.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aowow;
+
+if (!defined('AOWOW_REVISION'))
+    die('illegal access');
+
+
+class SoundPlaylistResponse extends TemplateResponse
+{
+    protected  string $template   = 'sound-playlist';
+    // js stuffs
+    protected  array  $breadcrumb = [0, 19, 1000];
+    protected ?int    $activeTab  = parent::TAB_DATABASE;
+    protected  string $pageName   = 'sound&playlist';
+    protected ?string $articleUrl = 'sound&playlist';
+
+    protected function generate() : void
+    {
+        $this->h1 = Lang::sound('cat', 1000);
+
+        array_unshift($this->title, $this->h1, Util::ucFirst(Lang::game('sound')));
+
+        parent::generate();
+    }
+}
+
+?>
