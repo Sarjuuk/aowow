40e98081c912761de6283d969c4682ee3118d792
Sarjuuk <junkdump@gmx.net>
2025-10-11 19:52:56 +0200

    LatestComments/RSS  * fix url format for replies

--- endpoints/latest-comments/latest-comments_rss.php
+++ endpoints/latest-comments/latest-comments_rss.php
@@ -14,15 +14,20 @@ class LatestcommentsRssResponse extends TextResponse
 
     protected function generate() : void
     {
-        foreach (CommunityContent::getCommentPreviews(dateFmt: false) as $comment)
+        foreach (CommunityContent::getCommentPreviews(['comments' => 1, 'replies' => 1], dateFmt: false) as $comment)
         {
+            if (empty($comment['commentid']))
+                $url = Cfg::get('HOST_URL').'/?go-to-comment&amp;id='.$comment['id'];
+            else
+                $url = Cfg::get('HOST_URL').'/?go-to-reply&amp;id='.$comment['id'];
+
             // todo (low): preview should be html-formated
             $this->feedData[] = array(
                 'title'       => [true,  [], Lang::typeName($comment['type']).Lang::main('colon').htmlentities($comment['subject'])],
-                'link'        => [false, [], Cfg::get('HOST_URL').'/?go-to-comment&amp;id='.$comment['id']],
+                'link'        => [false, [], $url],
                 'description' => [true,  [], htmlentities($comment['preview'])."<br /><br />".Lang::main('byUser', [$comment['user'], '']) . Util::formatTimeDiff($comment['date'])],
                 'pubDate'     => [false, [], date(DATE_RSS, $comment['date'])],
-                'guid'        => [false, [], Cfg::get('HOST_URL').'/?go-to-comment&amp;id='.$comment['id']]
+                'guid'        => [false, [], $url]
              // 'domain'      => [false, [], null]
             );
         }
